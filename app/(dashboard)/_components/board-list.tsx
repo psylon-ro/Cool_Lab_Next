f2c3018fefdef1e145379312f5f944f8ddc07bc1
Upendra Bhanushali <upendra99bhanushali@gmail.com>
2024-04-25 09:04:38 +0530

    check query params

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -35,10 +35,10 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
     );
   }
 
-  if (!data?.length && query.search) {
+  if (!data?.length && query.search) {//!data?.length && query.search
     return <EmptySearch />;
   }
-  if (!data?.length && query.favorites) {
+  if (!data?.length && query.favorites) {//!data?.length && query.favorites
     return <EmptyFavorites />;
   }
   if (!data?.length) {
